f5e1a62692266a217bac391d9d11b785a90d7229
bkavinoff@gmail.com <bkavinoff@gmail.com>
2017-07-07 14:01:18 -0300

    asd

---
+++ DemoWeb/App_Code/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Persona
+/// </summary>
+public class Persona
+{
+    private string nombre;
+
+    public Persona(string name)
+    {
+        this.nombre = name;
+    }
+
+    //PROPIEDAD es un set y get (acceso) de variables o atributos
+    public string Nombre {
+        get {
+            return this.nombre;
+        }
+        set {
+            this.nombre = value;
+        }
+    }
+}
