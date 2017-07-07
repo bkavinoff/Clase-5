27e57f861dbf4d4a2371ede0acd8e7cab706666a
bkavinoff@gmail.com <bkavinoff@gmail.com>
2017-07-07 11:56:20 -0300

    Creo Factory

---
+++ Demo Factory/LaFactoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Factory
+{
+    class LaFactoria : IPerreable
+    {
+        public void crearDogui(string dogui)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
